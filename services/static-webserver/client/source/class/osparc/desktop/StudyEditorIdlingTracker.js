3b82c49e1bb13062c5b86a7c518f414e60ac765a
Odei Maiz <maiz@itis.swiss>
2024-03-13 16:09:01 +0100

    DisplayName instead of oSPARC

--- services/static-webserver/client/source/class/osparc/desktop/StudyEditorIdlingTracker.js
+++ services/static-webserver/client/source/class/osparc/desktop/StudyEditorIdlingTracker.js
@@ -46,7 +46,7 @@ qx.Class.define("osparc.desktop.StudyEditorIdlingTracker", {
       }
 
       let msg = qx.locale.Manager.tr("Are you still there?") + "<br>";
-      msg += qx.locale.Manager.tr("If not, oSPARC will close the ") + osparc.product.Utils.getStudyAlias() + qx.locale.Manager.tr(" in: ");
+      msg += `If not, ${osparc.store.StaticInfo.getInstance().getDisplayName()} will try to close the ${osparc.product.Utils.getStudyAlias()} in:`;
       msg += osparc.utils.Utils.formatSeconds(timeoutSec);
       this.__idleFlashMessage.setMessage(msg);
     },
